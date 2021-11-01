ca540e2ca46bf68e586b812c880eed3e64843982
bielak7 <92972176+bielak7@users.noreply.github.com>
2021-11-01 14:52:46 +0100

    unnecessary headers removed

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import random
 
 user_wins = 0
-computer_wins =0
+computer_wins = 0
 
 options = ["rock", "paper", "scissors"]
-options[0]
+
 
 while True:
     user_input = input("Type rock/paper/scissors or Q to quit: ").lower()
@@ -16,25 +16,25 @@ while True:
 
     random_number = random.randint(0, 2)
 
-        #kamień: 0, papier:1,
+    # kamień: 0, papier:1,
     computer_pick = options[random_number]
     print("Computer picked", computer_pick + ".")
 
     if user_input == "rock" and computer_pick == "scissors":
-         print("You won!")
-         user_wins += 1
+        print("You won!")
+        user_wins += 1
 
     elif user_input == "paper" and computer_pick == "rock":
-         print("You won!")
-         user_wins += 1
+        print("You won!")
+        user_wins += 1
 
     elif user_input == "scissors" and computer_pick == "paper":
-         print("You won!")
-         user_wins += 1
+        print("You won!")
+        user_wins += 1
 
     else:
-         print("You lost")
-         computer_wins += 1
+        print("You lost")
+        computer_wins += 1
 
 print("You won", user_wins, "times.")
 print("the computer won", computer_wins, "times")
